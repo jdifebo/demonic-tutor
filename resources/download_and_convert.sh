f88565bf1e4b23ca94672e01e04711d8cc532646
Joe Di Febo <thegreatj03@gmail.com>
2019-12-21 11:02:01 -0600

    updating cards

--- resources/download_and_convert.sh
+++ resources/download_and_convert.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl -O https://mtgjson.com/json/AllCards.json
+curl -O https://www.mtgjson.com/json/AllCards.json
 curl -O https://www.mtgjson.com/json/SetList.json
+node simplifyPrices2.js > prices.json
+node simplifyPrices.js > SimplifiedPrices.json
 node convertCards.js > cards.json
 node extractSetInfo.js > sets_temp.json
 mv sets_temp.json sets.json
